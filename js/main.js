19e7d4ae990add051175087699abaf7b46d8532a
Time <75555299+tokihiko1229@users.noreply.github.com>
2021-04-27 21:18:08 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
   
   btn.addEventListener('click', () => {
     const n = Math.random();
-    if (n > 0.7) {
+    if (n > 0.6) {
       btn.textContent = '好きだってさ！';
     } else {
       btn.textContent = '一生独り身確定';
