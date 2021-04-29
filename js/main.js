aca0ab7650809fe6dcb05ee18582bb1670c7f42f
Time <75555299+tokihiko1229@users.noreply.github.com>
2021-04-29 13:19:02 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
 'use strict;'
 {
+  const input = document.querySelector('input');
   const btn = document.getElementById('btn');
-  
-  btn.addEventListener('click', () => {
-    const n = Math.random();
-    if (n >= 0.5) {
-      btn.textContent = 'バンちゃん';
+
+  const list = [];
+
+  document.getElementById('add').addEventListener('click', () => {
+    if(!input.value){
+      console.log('test');
     } else {
-      btn.textContent = 'ツル';
+      // const li = document.createElement('li');
+      // li.textContent = input.value;
+      // document.querySelector('ul').appendChild(li);
+      list.push(' ' + input.value + ' さん');
+      document.getElementById('member').textContent = '参加者一覧：' + list;
+      input.value = '';
+      input.focus();
+    }
+  });
+
+  btn.addEventListener('click', () => {
+    if(!list.length || list.length === 1){
+      btn.textContent = '参加者は2人以上です！';
+      document.getElementById('member').style.color = 'red';
+    } else {
+      const n = Math.floor(Math.random() * list.length);
+      btn.textContent = list[n] + 'の奢り！';
+      document.getElementById('member').style.color = 'black';
     }
   });
 }
